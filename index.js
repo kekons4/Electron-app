3bc07ebce81cea1fc21c3588e067ccfb2aff2da6
Keon Pourboghrat <kekons4@LAPTOP-NGRA9U3A.guestdomain>
2022-01-25 14:43:22 -0500

    first push of boiler plate code

---
+++ index.js
@@ -0,0 +1,68 @@
+const { app, BrowserWindow, Menu, MenuItem } = require('electron');
+const path = require('path');
+
+console.log(__dirname);
+let mainWindow;
+let page = './src/index.html';
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    });
+
+    mainWindow.loadFile(page);
+}
+
+//Create Main Menu
+const mainMenu = new Menu();
+
+// Add Main Menu item with help page
+mainMenu.append(new MenuItem({
+    label: 'Electron',
+    submenu: [{
+        role: 'help',
+        accelerator: process.platform === 'darwin' ? 'Cmd+H' : 'Control+H',
+        click() {
+            console.log('Keeks');
+            mainWindow.loadFile('./src/test.html');
+        }
+    }]
+}));
+
+// Add Dev tools Menu Item
+mainMenu.append(new MenuItem({
+    label: 'Developer Tools',
+    submenu: [{
+        role: 'toggleDevTools',
+        accelerator: process.platform === 'darwin' ? 'Cmd+Shift+I' : 'Control+Shift+I',
+        click() {
+            console.log('Opening Dev Tools');
+            if(process.NODE_ENV !== 'production') {
+                mainWindow.webContents.openDevTools();
+            }
+        }
+    }]
+}))
+
+// Applies Menu to the App
+Menu.setApplicationMenu(mainMenu);
+
+// When electron is ready
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+// When app is being closed 
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit();
+});
